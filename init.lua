4834bcf0fbf633ff8dd16a3ed9e3da01ced8c4de
stupienius <wangjay0401@gmail.com>
2025-04-20 12:32:45 +0800

    theme, highlight color, more beautiful completion menu

--- init.lua
+++ init.lua
@@ -2,13 +2,14 @@
 -- general --
 -------------
 
-vim.g.mapleader = " "
+vim.g.mapleader = " " -- use <space> as leader key
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
+vim.opt.tabstop = 2 -- 2-space tab
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus" -- use system clipboard as nvim clipboard
+vim.opt.pumheight = 8 -- popup up menu max items
 
 -- autocmd for c/c++ file to set 4-space tab
 vim.api.nvim_create_autocmd("FileType", {
@@ -29,12 +30,65 @@ vim.opt.rtp:prepend("~/.config/nvim/lazy/lazy.nvim")
 
 require("lazy").setup({
 
+	-- gruvbox for theme
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = true,
+					emphasis = true,
+					comments = true,
+					operators = true,
+					folds = true,
+				},
+				transparent_mode = true,
+			})
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+
+	-- gitsigns for signs
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({})
+		end,
+	},
+
+	-- nvim-highlight-colors for highlight color
+	{
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({
+				render = "background",
+				enable_hex = true,
+				enable_short_hex = true,
+				enable_rgb = true,
+				enable_hsl = true,
+				enable_var_usage = true,
+				enable_named_colors = true,
+				enable_tailwind = true,
+			})
+		end,
+		event = { "BufReadPre", "BufNewFile" },
+	},
+
 	-- nvim-tree for file tree
 	{
 		"nvim-tree/nvim-tree.lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" }, -- nvim-web-devicons for file icons
 		config = function()
-			require("nvim-tree").setup({})
+			require("nvim-tree").setup({
+				filters = {
+					dotfiles = true,
+				},
+			})
 		end,
 	},
 
@@ -77,8 +131,7 @@ require("lazy").setup({
 		end,
 	},
 
-	-- auto completion
-
+	-- nvim-cmp for auto completion
 	{
 		"neovim/nvim-lspconfig",
 		commit = "cb33dea", -- Last commit compatible with Neovim 0.9.5
@@ -86,19 +139,78 @@ require("lazy").setup({
 			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.ts_ls.setup({
+			lspconfig.ts_ls.setup({ -- TS JS
 				capabilities = capabilities,
 			})
 
-			lspconfig.clangd.setup({
+			lspconfig.clangd.setup({ -- c/c++
 				capabilities = capabilities,
 			})
 
-			lspconfig.pyright.setup({
+			lspconfig.pyright.setup({ -- python
 				capabilities = capabilities,
+			})
+
+			lspconfig.tailwindcss.setup({ -- tailwindcss
+				capabilities = capabilities,
+				filetypes = {
+					"html",
+					"css",
+					"scss",
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+					"vue",
+					"svelte",
+				},
+				init_options = {
+					userLanguages = {
+						eelixir = "html",
+						eruby = "html",
+					},
+				},
 			})
 		end,
 	},
+	{
+		"onsails/lspkind.nvim",
+		config = function()
+			require("lspkind").setup({
+				mode = "symbol_text",
+				preset = "codicons",
+
+				symbol_map = {
+					Text = "󰉿",
+					Method = "󰆧",
+					Function = "󰊕",
+					Constructor = "",
+					Field = "󰜢",
+					Variable = "󰀫",
+					Class = "󰠱",
+					Interface = "",
+					Module = "",
+					Property = "󰜢",
+					Unit = "󰑭",
+					Value = "󰎠",
+					Enum = "",
+					Keyword = "󰌋",
+					Snippet = "",
+					Color = "󰏘",
+					File = "󰈙",
+					Reference = "󰈇",
+					Folder = "󰉋",
+					EnumMember = "",
+					Constant = "󰏿",
+					Struct = "󰙅",
+					Event = "",
+					Operator = "󰆕",
+					TypeParameter = "",
+				},
+			})
+		end,
+	},
+
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
@@ -113,6 +225,15 @@ require("lazy").setup({
 			local luasnip = require("luasnip")
 
 			cmp.setup({
+				window = {
+					completion = {
+						border = "rounded",
+						winhighlight = "Normal:Normal,FloatBorder:CmpBorder,CursorLine:CmpSel,Search:None",
+					},
+					documentation = {
+						border = "rounded",
+					},
+				},
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
@@ -130,6 +251,20 @@ require("lazy").setup({
 					{ name = "buffer" },
 					{ name = "path" },
 				}),
+				formatting = {
+					format = function(entry, item)
+						local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+						item = require("lspkind").cmp_format({
+							ellipsis_char = "...", -- when popup menu exceed maxwis_char instead (must define maxwidth first)
+							show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+						})(entry, item)
+						if color_item.abbr_hl_group then
+							item.kind_hl_group = color_item.abbr_hl_group
+							item.kind = color_item.abbr
+						end
+						return item
+					end,
+				},
 			})
 		end,
 	},
